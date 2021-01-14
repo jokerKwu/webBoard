4e543fdf03e86df975826619063419985b5a83f8
joker <pkjhj485@naver.com>
2021-01-15 05:07:22 +0900

    웹소켓 이슈발생
    "추가 구성이없는 @EnableWebSocket은 작업 스케줄러의 다른 사용과 충돌하는 비 운영 스케줄러를 생성 할 수 있습니다."

--- src/main/java/com/joker/webchatting/springboot/config/WebSockConfig.java
+++ src/main/java/com/joker/webchatting/springboot/config/WebSockConfig.java
@@ -1,11 +1,16 @@
 package com.joker.webchatting.springboot.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import java.util.concurrent.Executors;
 
 @RequiredArgsConstructor
 @Configuration
@@ -18,5 +23,8 @@ public class WebSockConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
     }
-
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler(Executors.newSingleThreadScheduledExecutor());
+    }
 }
