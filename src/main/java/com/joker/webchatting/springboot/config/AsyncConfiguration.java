0a887d65b6d85274f25c0005016d432442b29d29
jokerKwu <south485@daum.net>
2021-01-14 15:54:17 +0900

    first commit

--- src/main/java/com/joker/webchatting/springboot/config/AsyncConfiguration.java
+++ src/main/java/com/joker/webchatting/springboot/config/AsyncConfiguration.java
@@ -20,7 +20,6 @@ public class AsyncConfiguration {
         taskExecutor.setThreadNamePrefix("Async-Executor-");
         taskExecutor.setDaemon(true);
         taskExecutor.initialize();
-
         return taskExecutor;
     }
 }
